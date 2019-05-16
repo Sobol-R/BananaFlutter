df4bf7e68bd2753dfb02d9af1fc462991d18d1da
Banandroid Z <ndrbnnz@gmail.com>
2019-05-16 21:05:22 +0300

    first commit

---
+++ lib/ui/map.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MapScreenState();
+}
+
+class MapScreenState extends State<MapScreen>  {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Привет"));
+  }
+}
